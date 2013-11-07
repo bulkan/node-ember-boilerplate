09f0925ebbb037e336851589068e4d12e51d5e4b
Bulkan Evcimen <bulkan@gmail.com>
2013-11-07 23:05:55 +1100

    Enable debugging

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,8 @@
-window.ToDo = Ember.Application.create({});
+ToDo = Ember.Application.create({
+  LOG_TRANSITIONS: true,
+  LOG_BINDINGS: true,
+  LOG_VIEW_LOOKUPS: true,
+  LOG_STACKTRACE_ON_DEPRECATION: true,
+  LOG_VERSION: true,
+  debugMode: true
+});
