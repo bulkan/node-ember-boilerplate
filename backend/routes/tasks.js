1665c9d04811aedf7465385258677ac306a4e4e4
Bulkan Evcimen <bulkan@gmail.com>
2013-11-14 23:13:14 +1100

    moved the `tasks` dummy data into a var
    this way we can simulate editing

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,15 +1,24 @@
+
+var tasks = {
+  tasks: [
+    {
+      id: 1,
+      title: 'Finish node-ember-boilerplate',
+      done: false
+    }, {
+      id: 2,
+      title: 'A dudd task',
+      done: true
+    }
+  ]
+};
+
 module.exports.index = function(req, res){
-  res.send({
-    tasks: [
-      {
-        id: 1,
-        title: 'Finish node-ember-boilerplate',
-        done: false
-      }, 
-      {
-        id: 2,
-        title: 'A dudd task',
-        done: true
-      }
-  ]});
+  res.send(tasks);
 }
+
+
+module.exports.edit = function(req, res) {
+  
+
+};
